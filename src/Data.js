16bd0f8b1a2c8a7c65f776db2188661081fa2a97
Kaustubh Chitravanshi <38200846+iamkaustubh1019@users.noreply.github.com>
2019-02-11 14:33:48 +0530

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -7,7 +7,7 @@ const Data = {
       staus: 'Open',
       address: 'IIIT-A, JhalwaCahuraha, Allahabad',
       tags: 'Dairy Products, Chocolates, Bread, Sanitizers, Shampoo',
-      image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg',
+      image: 'https://c8.alamy.com/comp/CXDHAX/inside-of-a-whisky-shop-whiskey-shops-scotland-CXDHAX.jpg',
 
       category: [
         {
@@ -21,35 +21,35 @@ const Data = {
           key: 'Fruits & Vegetables',
           id: 'Fruits & Vegetables',
           title: 'Fruits & Vegetables',
-          image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg',
+          image: 'http://www.newfoundbalance.com/wp-content/uploads/2016/11/vegetables-fruit-mixed-heart.jpg',
+        },
+
+        {
+          key: 'Fruits & Vegetables',
+          id: 'Fruits & Vegetables',
+          title: 'Utensils',
+          image: 'https://images-na.ssl-images-amazon.com/images/I/91aSbOYqFIL._SX466_.jpg',
         },
 
         {
           key: 'Fruits & Vegetables',
           id: 'Fruits & Vegetables',
           title: 'Fruits & Vegetables',
-          image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg',
+          image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2uN732mZgY4OUEbwzs0yxPUbXJFUnCXiKNDU1sjZxbYt1BVHj',
         },
 
         {
           key: 'Fruits & Vegetables',
           id: 'Fruits & Vegetables',
-          title: 'Fruits & Vegetables',
-          image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg',
+          title: 'Daily Products',
+          image: 'https://tuffclassified.com/oc-content/uploads/6335/575065.jpg',
         },
 
         {
           key: 'Fruits & Vegetables',
           id: 'Fruits & Vegetables',
-          title: 'Fruits & Vegetables',
-          image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg',
-        },
-
-        {
-          key: 'Fruits & Vegetables',
-          id: 'Fruits & Vegetables',
-          title: 'Fruits & Vegetables',
-          image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg',
+          title: 'Milk Products',
+          image: 'https://www.franchiseindia.com/uploads/news/ri/5ba5ef6ccbdfc.jpeg',
         },
       ],
 
@@ -57,29 +57,29 @@ const Data = {
         {
           key: 'Patanjali',
           id: 'Patanjali',
-          title: 'Patanjali',
-          image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg',
+          title: 'Himalay',
+          image: 'https://www.marketing91.com/wp-content/uploads/2018/01/SWOT-analysis-of-Himalaya-1.jpg',
+        },
+
+        {
+          key: 'Patanjali',
+          id: 'Patanjali',
+          title: 'Hindustan Uniliver',
+          image: 'https://yt3.ggpht.com/a-/AAuE7mB7T2D23S7cSrGsEwKe-lCo-dUT2RKMBz9R8A=s900-mo-c-c0xffffffff-rj-k-no',
         },
 
         {
           key: 'Patanjali',
           id: 'Patanjali',
           title: 'Patanjali',
-          image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg',
+          image: 'https://www.hindustantimes.com/rf/image_size_640x362/HT/p2/2016/09/10/Pictures/_2030fe72-774d-11e6-86aa-b218fe1cd668.jpg',
         },
 
         {
           key: 'Patanjali',
           id: 'Patanjali',
-          title: 'Patanjali',
-          image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg',
-        },
-
-        {
-          key: 'Patanjali',
-          id: 'Patanjali',
-          title: 'Patanjali',
-          image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg',
+          title: 'Reliance',
+          image: 'https://5.imimg.com/data5/TQ/PF/MY-6145389/reliance-digital-gift-voucher-500x500.jpg',
         },
       ],
 
@@ -92,29 +92,29 @@ const Data = {
           Description: 'This will keep you and your family heathy and safe',
           price: '300',
           discount: '0',
-          image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg'
+          image: 'https://4.imimg.com/data4/MU/OC/MY-3394706/patanjali-whole-wheat-atta-250x250.jpg'
         },
 
         {
           key: 'White Flour',
           id: 'White Flour',
           brand: 'Patanjali',
-          title: 'Patanjali White Flour',
+          title: 'Patanjali Toothpaste',
           Description: 'This will keep you and your family heathy and safe',
           price: '300',
           discount: '0',
-          image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg'
+          image: 'https://cf3.s3.souqcdn.com/item/2017/08/01/23/64/04/69/item_XL_23640469_33956618.jpg'
         },
 
         {
           key: 'White Flour',
           id: 'White Flour',
           brand: 'Patanjali',
-          title: 'Patanjali White Flour',
+          title: 'Patanjali Soap',
           Description: 'This will keep you and your family heathy and safe',
           price: '300',
           discount: '0',
-          image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg'
+          image: 'https://images-na.ssl-images-amazon.com/images/I/71xAGFgHAZL._SX355_.jpg'
         },
 
         {
